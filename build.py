95be09c71b3ad786bbeb8dd2afca228cbde71c0d
= <=>
2012-04-24 00:06:28 -0500

    On branch master I just added all the proper license tags to the files in this project!

--- build.py
+++ build.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python
+#/*
+#* WINHOMEFS This is a filesystem use for mouting
+#* and sharing a windows home directory with a linux
+#* instance without creating the big mess.
+#* 
+#* This file system is written by John-Charles D. Sokolow
+#* Copyright (C) 2011-1012 John-Charles D. Sokolow <john.charles.sokolow@gmail.com>
+#* 
+#* Based heavily on the example fuse file system "fusexmp_fh.c"
+#* Copyright (C) 2001-2007  Miklos Szeredi <miklos@szeredi.hu>
+#* 
+#* This program can be distributed under the terms of the GNU GPL.
+#* See the file COPYING.
+#* 
+#*/
 
 import subprocess, sys, os, shutil
 
--- utilities.c
+++ utilities.c
@@ -1,3 +1,18 @@
+/*
+ * WINHOMEFS This is a filesystem use for mouting
+ * and sharing a windows home directory with a linux
+ * instance without creating the big mess.
+ * 
+ * This file system is written by John-Charles D. Sokolow
+ * Copyright (C) 2011-1012 John-Charles D. Sokolow <john.charles.sokolow@gmail.com>
+ * 
+ * Based heavily on the example fuse file system "fusexmp_fh.c"
+ * Copyright (C) 2001-2007  Miklos Szeredi <miklos@szeredi.hu>
+ * 
+ * This program can be distributed under the terms of the GNU GPL.
+ * See the file COPYING.
+ * 
+ */
 #include "utilities.h"
 
 #include <stdio.h>
